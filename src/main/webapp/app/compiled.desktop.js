a05ae09ff90b285815880affc5db6d1c3387c6e8
Clement <clement.hannicq@revevol.eu>
2014-06-26 10:47:07 +0200

    Implemented a custom multi select to be able to display colors on desktop

--- src/main/webapp/app/compiled.desktop.js
+++ src/main/webapp/app/compiled.desktop.js
@@ -2639,3 +2639,45 @@ location.href = a;
 }
 };
 });
+calApp.directive('devicePicker', function() {
+return {
+templateUrl: '/directives/devicepicker/devicepicker.html',
+restrict: 'EA',
+scope: {
+filter: "="
+},
+controller: function($scope, endpoint) {
+$scope.devices = [];
+$scope.filter = [];
+
+$scope.background = function(){
+if(this.d.selected){
+return "rgba("+this.d.r+","+this.d.g+","+this.d.b+",0.5)";
+} else {
+return "inherit";
+}
+};
+
+$scope.click = function(){
+this.d.selected = !this.d.selected;
+if(this.d.selected){
+$scope.filter.push(this.d.id);
+} else {
+var temp = [];
+for(var i = 0; $scope.filter.length > i; i++){
+if($scope.filter[i] !== this.d.id){
+temp.push($scope.filter[i]);
+}
+}
+$scope.filter = temp;
+}
+};
+
+endpoint.then(function(endpoint) {
+endpoint.asset.list().success(function(data){
+$scope.devices = data.items;
+});
+});
+}
+};
+});

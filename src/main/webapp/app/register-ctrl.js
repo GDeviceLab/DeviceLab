21740a77797d79bf880d4ab4256b457d8a578cd3
Emiliano Piccinini <emiliano.piccinini@revevol.eu>
2015-01-23 12:36:50 +0100

    fix #52

--- src/main/webapp/app/register-ctrl.js
+++ src/main/webapp/app/register-ctrl.js
@@ -10,22 +10,22 @@ calApp.controller("RegisterCtrl", function($scope, endpoint, $window) {
     
     endpoint.then(function(endpoint) {
         $scope.show = endpoint.show()
-
+        
         $scope.loading = function() {
             return endpoint.loading();
         }
         $scope.submit = function() {
             $scope.error.mandatoryMessage = false;
             if($scope.name != null
-                 && $scope.name.trim() != ""
-                 && $scope.startupName != null
-                 && $scope.startupName.trim() != ""){
+                    && $scope.name.trim() != ""
+                    && $scope.startupName != null
+                    && $scope.startupName.trim() != ""){
                 endpoint.person.register($scope.name,$scope.startupName)
-                    .success(function() {
-                        $window.location.hash = "/apply"
-                        $scope.show = false;
-                        ndp.callMe();
-                    });
+                        .success(function() {
+                            $window.location.hash = "/apply"
+                    $scope.show = false;
+                    ndp.callMe();
+                });
             }
             else{
                 $scope.error.mandatoryMessage = true;
@@ -43,14 +43,20 @@ calApp.controller("TitleCtrl", function($scope, endpoint, $window) {
         if(loc.length < 2){
             return "home";
         } else {
+            if(loc[1] === 'news'){
+                if(loc[2] != null
+                    && loc[2] == "new"){
+                    $scope.page = "Add News";
+                }
+                else{
+                    $scope.page = "News";
+                }
+            }
             return loc[1];
         }
     }, function(n){
         var value = n.charAt(0).toUpperCase() + n.slice(1);
-        if(value == 'News'){
-            value = 'Add News';
-        }
-        else if(value == 'Stats'){
+        if(value == 'Stats'){
             value = 'My Stats';
         }
         $scope.page = value;
